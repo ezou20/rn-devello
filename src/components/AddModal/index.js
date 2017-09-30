a08fd1a3313009dbe62834474ca8b7bd0e06f89c
Dong Hur <dhur@college.harvard.edu>
2017-09-30 13:13:49 -0400

    deletes App component and cleans up Addmodl component

--- src/components/AddModal/index.js
+++ src/components/AddModal/index.js
@@ -3,15 +3,10 @@
 // import { connect } from 'react-redux'
 // import { bindActionCreators } from "redux"
 // import { Constants } from "expo"
-// import Button from "../Button"
-// import { addBoard, addList, addCard } from "../../redux/actions"
 
-// const mapStateToProps = state => ({ visible: state.visible, comp: state.comp, parent: state.parent })
-// const mapDispatchToProps = dispatch => ({
-//   addBoard: bindActionCreators(addBoard, dispatch),
-//   addList: bindActionCreators(addList, dispatch),
-//   addCard: bindActionCreators(addCard, dispatch)
-// })
+// // get redux state and functions here
+// const mapStateToProps = state => ({})
+// const mapDispatchToProps = {}
 
 // class AddModal extends Component {
 //   constructor(props) {
@@ -22,89 +17,30 @@
 //   }
 
 //   addBoard = () => {
-//     this.props.addBoard(this.state.input)
-//     this.props.onCancel()
+//     console.log('Adding Board')
 //   }
 
 //   addList = () => {
-//     this.props.addList(this.state.input, this.props.parent)
-//     this.props.onCancel()
+//     console.log('Adding List')
 //   }
 
 //   addCard = () => {
-//     this.props.addCard(this.state.input, this.props.parent)
-//     this.props.onCancel()
+//     console.log('Adding Card')
 //   }
 
 //   render() {
-//     let str = `Create ${this.props.comp}`
-//     let button
-//     if (this.props.comp === "Board") button = <Button onClick={this.addBoard} text={str} style="create" textColor="white" />
-//     else if (this.props.comp === "List") button = <Button onClick={this.addList} text={str} style="create" textColor="white" />
-//     else if (this.props.comp === "Card") button = <Button onClick={this.addCard} text={str} style="create" textColor="white" />
 //     return (
 //       <Modal
 //         animationType={"fade"}
 //         transparent={true}
 //         visible={this.props.visible}
 //         >
-//           <View style={[styles.modal]}>
-//             <View style={[styles.innerContainer]}>
-//               <Text style={styles.header}>{this.props.comp}</Text>
-//               <TextInput
-//                 style={styles.inputs}
-//                 clearTextOnFocus={true}
-//                 onChangeText={input => {
-//                   this.setState({ input })
-//                 }}
-//                 value={this.state.input}
-//               />
-//               <View style={styles.buttons}>
-//                 <Button onClick={this.props.onCancel} text="Cancel" style="cancel" textColor="white" />
-//                 {button}
-//               </View>
-//             </View>
+//           <View>
+//             This Is a Modal
 //           </View>
 //       </Modal>
 //     )
 //   }
 // }
-
-// const styles = StyleSheet.create({
-//   inputs: {
-//     height: 40,
-//     borderColor: "gray",
-//     borderRadius: 5,
-//     borderWidth: 1,
-//   },
-//   view: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     marginTop: Constants.statusBarHeight
-//   },
-//   innerContainer: {
-//     borderRadius: 10,
-//     alignItems: 'center',
-//     backgroundColor: '#fff',
-//     padding: 20,
-//   },
-//   modal: {
-//     display: 'flex',
-//     flex: 1,
-//     justifyContent: 'center',
-//     padding: 20,
-//     backgroundColor: 'rgba(0, 0, 0, 0.5)',
-//   },
-//   header: {
-//     fontSize: 20,
-//     textAlign: "center",
-//     marginBottom: 10,
-//   },
-//   buttons: {
-//     display: 'flex',
-//     flexDirection: 'row',
-//     marginTop: 20,
-//   }
-// })
 
 // export default connect(mapStateToProps, mapDispatchToProps)(AddModal)
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-export {default as App} from './App'
 export {default as Button} from './Button'
 export {default as Board} from './Board'
 export {default as Card} from './Card'
